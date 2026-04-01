86e50de48e6b0da8c1b39875b58f515e8813add0
SAWADOGO943 <stevenyoussoufs@gmail.com>
2026-04-01 19:29:53 +0000

    script

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,9 @@ const sendBtn      = document.getElementById('sendBtn');
 const loader       = document.getElementById('loader');
 
 // URL du backend — à modifier si tu changes de port ou déploies
-const API_URL = "https://AI-agent-backend.onrender.com/chat";
+const API_URL = "https://ai-agent-13vc.onrender.com";
+
+
 
 
 // ── FONCTION : Afficher une bulle de message ────────────────────
